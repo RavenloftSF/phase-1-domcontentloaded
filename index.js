01262bdc3be92f002e85ce47d03a5baa64f48b45
JosephBorrelli <josephborrelli80@gmail.com>
2023-07-10 12:33:50 -0400

    blurp

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    let pTag = document.getElementById("text")
+    pTag.innerHTML = ("This is really cool!")
+  });
+  
+  console.log("This is really cool!");
